99f3fdd95858007ee57738f5f7c598c0efef7df9
hdmamin <hmamin2@dons.usfca.edu>
2021-04-22 20:42:35 -0700

    bigram based realign punctuated text func

--- lib/jabberwocky/youtube.py
+++ lib/jabberwocky/youtube.py
@@ -1,4 +1,7 @@
+from fuzzywuzzy import fuzz
+import numpy as np
 import pandas as pd
+import re
 from youtube_transcript_api import YouTubeTranscriptApi
 
 from htools import DotDict
@@ -85,3 +88,65 @@ def get_transcripts(url, verbose=True):
         print(f'The following keys are null: {repr(null_keys)}')
     return DotDict(**results, id=id_)
 
+
+def realign_punctuated_text(df, text, skip_1st=1, margin=2):
+    """Realign gpt3's punctuated version of a youtube transcript with the
+    original timestamps.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Auto-generated transcript from YouTubeTranscriptApi. Contains 'text',
+        'start', and 'duration' columns.
+    text: str
+        Punctuated text returned by GPT3.
+    skip_1st: int
+        Number of words at the start of each punctuated chunk to skip. The
+        minimum number of words per row will be skip_1st+2. This helps protect
+        slightly against cases where the true line is a common sequence that
+        we encounter at the very start of the candidate
+        Ex:
+        unpunctuated: 'it is what it is'
+        punctuated: 'It is what it is.'
+
+        With skip_1st=0, we may mistakenly select "It is" from the punctuated
+        candidate. With skip_1st=1, we select "It is what it is." as desired.
+    margin: int
+        Number of additional words to consider when constructing a punctuated
+        chunk (list) from our punctuated text.
+
+        Ex (margin=2):
+        unpunctuated: ['this', 'is', 'true', 'however', 'we']
+        punctuated: ['This', 'is', 'true', '-', 'however', 'we', 'still']
+        Notice that adding punctuation can create new tokens, so with margin=0
+        our candidate would be truncated prematurely (ending with 'however').
+    """
+    # Built-in str.split doesn't retain starting/trailing spaces correctly.
+    # Probably would be fine but just keep this since it took a while to get
+    # right and I don't want to break it.
+    words = re.split(' ', text)
+    rows = []
+    start_i = 0
+    for i, chunk in df.iterrows():
+        chunk_words = re.split(' ', chunk.text)
+        length = len(chunk_words)
+        punct_words = words[start_i:start_i + length + margin]
+        suff = ' '.join(chunk_words[-2:])
+        scores = []
+        bigrams = zip(punct_words[skip_1st:], punct_words[skip_1st + 1:])
+        # Avoid list comp so we can exit early if we find a perfect match.
+        for i, gram in enumerate(bigrams):
+            score = fuzz.ratio(suff, ' '.join(gram))
+            if score == 100:
+                argmax = i
+                break
+            scores.append(score)
+        else:
+            argmax = np.argmax(scores)
+        punct_len = skip_1st + argmax + 2
+        rows.append(' '.join(words[start_i:start_i + punct_len]))
+        start_i += punct_len
+
+    new_df = pd.DataFrame(rows, columns=['text'])
+    return pd.concat((new_df, df.reset_index()[['start', 'duration']].copy()),
+                     axis=1)
