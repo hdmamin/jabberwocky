7d28b538956e07160042297e2c90d5fc57a10741
hdmamin <hmamin2@dons.usfca.edu>
2021-09-02 20:59:06 -0700

    py2app setup.py builds in alias mode but no regular mode

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import sys
+
+
+sys.setrecursionlimit(2_500)
+OPTIONS = {
+    'argv_emulation': False,
+    'plist': {
+        'PyRuntimeLocations':
+        ['@executable_path/../Frameworks/libpython3.7m.dylib',
+         '/Users/hmamin/anaconda3/lib/libpython3.7m.dylib']
+    }
+}
+
+setup(
+    app=['bin/main.py'],
+    data_files=['data'],
+    setup_requires=['py2app'],
+    options={'py2app': OPTIONS}
+)
