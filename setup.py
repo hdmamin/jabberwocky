2d80a0383420d22caa1a50b1411920e7e26aa067
hdmamin <hmamin2@dons.usfca.edu>
2021-09-03 21:16:42 -0700

    add

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import sys
 sys.setrecursionlimit(2_500)
 OPTIONS = {
     'argv_emulation': False,
+    'iconfile': 'data/icons/icon.icns',
     'plist': {
         'PyRuntimeLocations':
         ['@executable_path/../Frameworks/libpython3.7m.dylib',
