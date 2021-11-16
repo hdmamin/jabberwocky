9f3e6654da6bde7d2dd8cec07cecf5991e819029
hdmamin <hmamin2@dons.usfca.edu>
2021-11-15 22:00:01 -0800

    notes

--- lib/jabberwocky/cli.py
+++ lib/jabberwocky/cli.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 from prompt_toolkit import prompt, PromptSession
 
-from jabberwocky.openai_utils import ConversationManager, PromptManager
+from jabberwocky.openai_utils import ConversationManager, PromptManager,\
+    query_gpt3, query_gpt_neo, query_gpt_j
 from htools.cli import fire
 from htools.core import eprint, identity
 
@@ -15,7 +16,7 @@ def strip_and_lower(text):
 
 
 def prompt_until_valid(valid_responses, msg,
-                       error_msg='Unrecognized input: {}.',
+                       error_msg='Unrecognized input: {}',
                        postprocess=str.strip, sess=None):
     sess = sess or PromptSession(vi_mode=True)
     if not postprocess: postprocess = identity
@@ -30,20 +31,25 @@ def prompt_until_valid(valid_responses, msg,
 
 
 # TODO
-def talk(name, model, download=False):
-    conv = ConversationManager()
+def talk(name, model='gptj', download_persona=False,
+         engine_i=0, temperature=0.7, frequency_penalty=0.0, max_tokens=50,
+         **query_kwargs):
+    query_kwargs = dict(engine_i=engine_i,
+                        temperature=temperature,
+                        frequency_penalty=frequency_penalty,
+                        max_tokens=max_tokens,
+                        **query_kwargs)
+    if model in ('gpt_j', 'gpt_neo'):
+        query_kwargs['mock_func'] = globals()[f'query_{model}']
+    conv = ConversationManager(verbose=False)
     sess = PromptSession(vi_mode=True)
-    conv.start_conversation(name, download_if_necessary=download)
-    query = None
+    conv.start_conversation(name, download_if_necessary=download_persona)
     while True:
-        query = sess.prompt('Me: ').strip()
-        print('-' * 79)
-        print('COMMAND:')
-        print(query)
-        print('-' * 79)
+        query = sess.prompt('\nMe: ').strip()
         if query == '/quit':
             cmd = prompt_until_valid(
-                {'y', 'n'}, 'Would you like to save this conversation? [y/n]',
+                {'y', 'n'},
+                'Would you like to save this conversation? [y/n]\n',
                 postprocess=strip_and_lower, sess=sess
             )
             if cmd == 'y':
@@ -54,8 +60,10 @@ def talk(name, model, download=False):
                 conv.end_conversation()
                 return
         else:
-            input_, resp = conv.query(cmd)
-            print(resp)
+            print(query_kwargs)
+            _, resp = conv.query(query, **query_kwargs)
+            print(f'\n{conv.process_name(conv.current_persona, inverse=True)}'
+                  f': {resp}')
 
 
 def task():
