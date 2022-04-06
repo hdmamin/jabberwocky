af897bf89db5cafad870c03c9a5c431393824a2d
hdmamin <hmamin2@dons.usfca.edu>
2022-04-05 20:11:02 -0700

    update hook

--- pre-commit.py
+++ pre-commit.py
@@ -1,15 +1,16 @@
 import subprocess
+import sys
 
 
 OPENAI_KEY_PATTERN = 'sk-[a-zA-Z0-9]{48}'
 
 
 def main():
-    print(__file__)
+    print('In commit hook', __file__)
     res = subprocess.run(['ack',
                           OPENAI_KEY_PATTERN,
                           '/Users/hmamin/jabberwocky/'])
-    return 1 - res.returncode
+    sys.exit(1 - res.returncode)
 
 
 if __name__ == '__main__':
