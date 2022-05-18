9bd853cd39cd86043b7227dd42dada2fd996091a
hdmamin <hmamin2@dons.usfca.edu>
2022-05-17 20:30:23 -0700

    use outlook email

--- alexa/config.py
+++ alexa/config.py
@@ -1,6 +1,6 @@
 from jabberwocky.utils import load_huggingface_api_key
 
 
-EMAIL = 'jabberwocky.alexa@gmail.com'
+EMAIL = 'jabberwocky-alexa@outlook.com'
 LOG_FILE = 'alexa/app.log'
 HF_API_KEY = load_huggingface_api_key()
