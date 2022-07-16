f1968b1a02a84d88dbea3f6e4f07dd27fc3591d2
hdmamin <hmamin2@dons.usfca.edu>
2022-07-15 20:31:32 -0700

    add msg to pre commit hook

--- pre-commit.py
+++ pre-commit.py
@@ -21,6 +21,7 @@ def error(text):
 
 def main():
     # Piping in subprocess requires str arg instead of list and shell=True.
+    print('Checking committed files for openai API keys...')
     git_matches = subprocess.run(
         f'git ls-files | ack -x {OPENAI_KEY_PATTERN}',
         shell=True
@@ -31,13 +32,14 @@ def main():
         error('\nERROR: FOUND POSSIBLE EXPOSED API KEY. \nCommit aborted. '
               'Use the `--no-verify` to force commit anyway.\n')
     else:
+        print('Checking all files for openai API keys...')
         all_matches = subprocess.run(
             ['ack', OPENAI_KEY_PATTERN, Path('~/jabberwocky/').expanduser()]
         )
         if not all_matches.returncode:
             warn('\nWARNING: FOUND POSSIBLE EXPOSED API KEY. \nAllowing '
-                  'commit to proceed because it doesn\'t appear to be in a '
-                  'file you\'ve committed to git, but be very careful.')
+                 'commit to proceed because it doesn\'t appear to be in a '
+                 'file you\'ve committed to git, but be very careful.')
     sys.exit(code)
 
 
